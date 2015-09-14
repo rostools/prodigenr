2a8ee1313bee3de2db98282e01479253099b49aa
Luke W Johnston <lwjohnst@gmail.com>
2015-09-14 19:52:39 -0400

    Added exports to some functions

--- R/getConfigData.R
+++ R/getConfigData.R
@@ -10,21 +10,25 @@ NULL
 
 
 ##' @rdname getConfigData
+##' @export
 getBib <- function(config.file) {
     yaml::yaml.load_file(config.file)$bib_file
 }
 
 ##' @rdname getConfigData
+##' @export
 getCSL <- function(config.file) {
     yaml::yaml.load_file(config.file)$csl_file
 }
 
 ##' @rdname getConfigData
+##' @export
 getData <- function(config.file) {
     yaml::yaml.load_file(config.file)$data_file
 }
 
 ##' @rdname getConfigData
+##' @export
 getAuthor <- function(config.file) {
     yaml::yaml.load_file(config.file)$author
 }
--- R/loadData.R
+++ R/loadData.R
@@ -7,7 +7,7 @@
 loadData <- function(path) {
     data.files <- list.files(path, pattern = '*.RData$', ignore.case = TRUE)
     data.exists <- length(which(file.exists(data.files)))
-    if (data.exists > 1) {
+    if (data.exists >= 1) {
         sapply(data.files, load, .GlobalEnv)
     } else {
         stop('Please fetch or save your data in the data folder.')
