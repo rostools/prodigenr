48178587fc3894c8c8e1bb87f57e987caa7e7c8d
Luke W Johnston <lwjohnst@gmail.com>
2015-10-26 22:01:16 -0400

    fully developed poster project

--- inst/templates/projects/poster/src/fetchData.R
+++ inst/templates/projects/poster/src/fetchData.R
@@ -1,19 +1,21 @@
 ##' ---
 ##' title: Fetch and Process the Data
-##' author: `r getAuthor(
 ##' ---
-##' 
+##'
 ##' This script fetchs the main dataset, keeps variables relevant to
 ##' the analysis, restrict the sample size as needed, and lastly save
 ##' the new dataset as an `.RData` file.
-##' 
+##'
 ##' Load the options for the R session.
-runSetup('./')
+prodigenr::runSetup('./')
+data_file <- 'path/to/data'
 
-##' Load the master dataset, 
-ds.prep <- tbl_df(read.table(getData('../config.yaml'),
-                             header = TRUE, na = "",
-                             sep = ","))
+##' Load the master dataset,
+ds.prep <- tbl_df(read.table(
+    data_file,
+    header = TRUE, na = "",
+    sep = ","
+))
 
 ds <- ds.prep
 
@@ -22,5 +24,7 @@ rm(list=setdiff(ls(), "ds"))
 
 ##' Save the dataset as an RData file.  Create a data directory in
 ##' case none exists.
-saveRDS(ds, file = file.path('..', 'data', 'dataset.RData')) ## Name and location to save dataset
+## Name and location to save dataset
+dir.create(file.path('..', 'data'))
+save(ds, file = file.path('..', 'data', 'dataset.RData'))
 
