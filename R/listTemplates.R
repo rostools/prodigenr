fe89f84194e8a7b776233cabe61c13e0e194566f
Luke W Johnston <lwjohnst@gmail.com>
2015-10-26 21:45:18 -0400

    Cleaned up/simplified listtemplates

--- R/listTemplates.R
+++ R/listTemplates.R
@@ -6,15 +6,18 @@
 ##' @return Vector of templates available
 ##' @export
 ##' @author Luke W. Johnston
-listTemplates <- function(type = c('files', 'projects'), package = 'prodigenr') {
-    if (length(nchar(type)) > 1) {
-        stop('Use either "files" or "projects" for type.')
-    } else if (type == 'files') {
-        list.files(system.file('templates', type, package = package), '\\w$')
-    } else if (type == 'projects') {
-        list.dirs(system.file('templates', type, package = 'prodigenr'),
+##' @example
+##'
+##' listTemplates('files')
+##' listTemplates('projects')
+##' listTemplates()
+##'
+listTemplates <- function(type = c('projects', 'files'), package = 'prodigenr') {
+    temp.type <- match.arg(type)
+    if (temp.type == 'files') {
+        list.files(system.file('templates', temp.type, package = package), '\\w$')
+    } else if (temp.type == 'projects') {
+        list.dirs(system.file('templates', temp.type, package = 'prodigenr'),
                   full.names = FALSE, recursive = FALSE)
-    } else {
-        stop('Use either "files" or "projects" for type.')
     }
 }
