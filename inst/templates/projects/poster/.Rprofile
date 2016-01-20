59201946000032654bf1d42f6653d458200b06db
Luke W Johnston <lwjohnst@gmail.com>
2016-01-20 14:09:21 -0500

    Resolved #3

---
+++ inst/templates/projects/poster/.Rprofile
@@ -0,0 +1,3 @@
+cat('Sourcing files in the R/ folder...\n')
+invisible(sapply(list.files('R', pattern = '*.R', full.names = TRUE), source))
+cat('...Loaded.')
--- inst/templates/projects/slides/R/setup.R
+++ inst/templates/projects/slides/R/setup.R
@@ -4,23 +4,34 @@
 #' Install packages if they are not installed and load them. Set options for
 #' the packages.
 #'
+#' *No need to edit*
 run_setup <- function() {
     # Run the following packages for setup
     .load_packages()
     .set_options()
+    # Run twice just in case some weren't loaded.
+    .load_packages()
 }
 
+# Edit as needed
 .load_packages <- function() {
     # Add your packages you want to use here, using the following format:
+    # - For pre-bundled packages:
+    # library(package)
+
+    # - For CRAN packages:
     # if (!require(package)) install.packages('package')
-    # Or, for github packages:
+
+    # - For github packages:
     # if (!require(package)) packrat::install_github('username/package')
+
 }
 
+# Edit as needed
 .set_options <- function() {
     # Set the options here for individual packages
 
-    # For tables (pander)
+    # For tables (uncomment below if using pander)
 #     panderOptions('table.split.table', Inf)
 #     panderOptions('table.style', 'rmarkdown')
 #     panderOptions('table.alignment.default',
@@ -28,8 +39,8 @@ run_setup <- function() {
 #                       ifelse(sapply(df, is.numeric), 'center', 'left'))
 
     # For the document (knitr)
-#     knitr::opts_chunk$set(
-#         warning = FALSE, message = FALSE, collapse = TRUE
-#     )
+    knitr::opts_chunk$set(
+        warning = FALSE, message = FALSE, collapse = TRUE
+    )
 }
 
