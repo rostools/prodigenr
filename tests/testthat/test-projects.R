b94dac4107c29867230508be783a1732af6cf119
Luke W Johnston <lwjohnst@gmail.com>
2016-05-11 14:56:36 -0400

    Wrote a unit test for checking that all files were created

--- tests/testthat/test-projects.R
+++ tests/testthat/test-projects.R
@@ -3,7 +3,7 @@ context('projects')
 # Maybe reorg this so that prodigen project is created, then check that all
 # conditions are met...
 
-test_that("project directories created (no git or packrat)", {
+test_that("project directory and files created (no git or packrat)", {
 
     skip_on_cran()
     #skip_on_travis()
@@ -11,27 +11,27 @@ test_that("project directories created (no git or packrat)", {
     testProjects <- function(type, name = NULL) {
         if (is.null(name)) name <- type
         output.dir <- tempdir()
-        # expected.files <- list(
-        #     base = c('.Rprofile', paste0(name, '.Rproj'), 'README.md'),
-        #     R = c('functions.R', 'setup.R', 'load_data.R', 'fetch_data.R')
-        # )
-        # rmd.file <- paste0(type, '.Rmd')
-        # switch(type,
-        #        manuscript = {
-        #            expected.files$doc <-  rmd.file
-        #        },
-        #        slides = {
-        #            expected.files$base <- c(expected.files$base, 'template.tex',
-        #                                     rmd.file)
-        #        },
-        #        poster = {
-        #            expected.files$base <- c(expected.files$base, 'template.tex',
-        #                                   'beamerthemeCustomPoster.sty',
-        #                                   rmd.file)
-        #        },
-        #        abstract = {
-        #            expected.files$base <- c(expected.files$base, rmd.file)
-        #        })
+        expected.files <- list(
+            base = c('.Rprofile', paste0(name, '.Rproj'), 'README.md'),
+            R = c('functions.R', 'setup.R', 'load_data.R', 'fetch_data.R')
+        )
+        rmd.file <- paste0(type, '.Rmd')
+        switch(type,
+               manuscript = {
+                   expected.files$doc <-  rmd.file
+               },
+               slides = {
+                   expected.files$base <- c(expected.files$base, 'template.tex',
+                                            rmd.file)
+               },
+               poster = {
+                   expected.files$base <- c(expected.files$base, 'template.tex',
+                                          'beamerthemeCustomPoster.sty',
+                                          rmd.file)
+               },
+               abstract = {
+                   expected.files$base <- c(expected.files$base, rmd.file)
+               })
 
         prodigen(proj.type = type,
                  proj.name = name,
@@ -41,22 +41,22 @@ test_that("project directories created (no git or packrat)", {
         project.files <- sort(basename(list.files(project.dir, recursive = TRUE,
                                                   all.files = TRUE, no.. = TRUE,
                                                   ignore.case = TRUE)))
-        # expected.files <- sort(unname(unlist(expected.files)))
-        #
-        # differences <- setdiff(project.files, expected.files)
-        # if (length(differences) > 0)
-        #     print(paste(differences))
+        expected.files <- sort(unname(unlist(expected.files)))
+
+        differences <- setdiff(project.files, expected.files)
+        if (length(differences) > 0)
+            print(paste(differences))
 
         expect_true(dir.exists(project.dir))
         expect_true(dir.exists(file.path(project.dir, 'R')))
-        # expect_identical(project.files, expected.files)
+        expect_identical(project.files, expected.files)
 
         if (type == 'manuscript') {
             expect_true(dir.exists(file.path(project.dir, 'doc')))
         }
 
         unlink(project.dir, recursive = TRUE)
-        # project.files
+        project.files
     }
 
     testProjects('manuscript')
