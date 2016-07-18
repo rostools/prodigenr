c7087290a499d12a2150438c06f4bed60d94b234
Luke W Johnston <lwjohnst@gmail.com>
2016-07-18 15:33:29 -0400

    Resolves #9, resolves #12

--- vignettes/introduction.R
+++ vignettes/introduction.R
@@ -1,15 +1,29 @@
+## ---- echo = FALSE-------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "README-"
+)
+
 ## ----templates, collapse=TRUE--------------------------------------------
 library(prodigenr)
-list_templates()
+template_list
+path <- tempdir()
 
-## ---- eval=FALSE---------------------------------------------------------
-#  prodigen('manuscript', 'ManuscriptName', '~/path/to/new/project')
+## ----manuscriptProj------------------------------------------------------
+prodigen('manuscript', 'ManuscriptName', path, git.init = TRUE)
+
+## ----shell_command, eval = FALSE, echo = FALSE---------------------------
+#  # run only on computer
+#  cat(paste(system(paste0('cd ', path, '/ManuscriptName && tree'), intern = TRUE), collapse = '\n'),
+#      file = 'vignettes/file_structure.txt')
+
+## ----file_structure, echo = FALSE, results='markup', comment=''----------
+unlink(file.path(path, 'ManuscriptName'))
+cat(readLines('file_structure.txt', warn = FALSE), sep = '\n')
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  prodigen('manuscript', 'ManuscriptName', '~/path/to/new/project', git.init = TRUE)
-
-## ---- eval=FALSE---------------------------------------------------------
-#  prodigen('manuscript', 'ManuscriptName', '~/path/to/new/project', packrat.init = TRUE)
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  prodigen('slides', 'PresentationName', '~/path')
