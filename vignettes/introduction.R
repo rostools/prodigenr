41121253b37cf39e029b51b7542311030adbfcfe
Luke W Johnston <lwjohnst@gmail.com>
2016-07-28 22:01:16 -0400

    Updated vignette file with new style

--- vignettes/introduction.R
+++ vignettes/introduction.R
@@ -5,15 +5,15 @@ knitr::opts_chunk$set(
   eval = FALSE
 )
 
-## ----templates, collapse=TRUE--------------------------------------------
-#  library(prodigenr)
-#  template_list
-#  path <- tempdir()
+## ----templates, collapse=TRUE, eval=TRUE---------------------------------
+library(prodigenr)
+path <- tempdir()
+template_list
 
 ## ----manuscriptProj------------------------------------------------------
 #  prodigen('manuscript', 'ManuscriptName', path, git.init = TRUE)
 
-## ----shell_command, echo = FALSE-----------------------------------------
+## ----shell_command, echo=FALSE-------------------------------------------
 #  # run only on computer
 #  cat(paste(system(
 #      paste0('cd ', path, '/ManuscriptName && tree -a -I .git --dirsfirst -v'),
@@ -22,10 +22,15 @@ knitr::opts_chunk$set(
 #      file = 'vignettes/file_structure.txt')
 #  unlink(file.path(path, 'ManuscriptName'), recursive = TRUE)
 
-## ----file_structure, eval = TRUE, echo = FALSE, results='markup', comment=''----
+## ----file_structure, echo=FALSE, eval=TRUE, results='markup', comment=''----
 cat(readLines('file_structure.txt', warn = FALSE), sep = '\n')
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  prodigen('slides', 'PresentationName', '~/path')
 #  prodigen('abstract', 'Name', '.') # Current directory
+
+## ------------------------------------------------------------------------
+#  prodigenr::include_rfigshare_script()
+#  prodigenr::include_mit_license()
+#  prodigenr::include_strobe()
 
