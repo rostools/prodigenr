4caf84c6e80df7f37603348303beb054547b56b6
Luke W Johnston <lwjohnst@gmail.com>
2018-05-12 22:07:56 +0200

    remove extra code

--- R/utils.R
+++ R/utils.R
@@ -42,34 +42,11 @@ done <- function(...) {
   bullet(paste0(...), bullet = crayon::green(clisymbols::symbol$tick))
 }
 
-code_block <- function(..., copy = interactive()) {
-  block <- paste0("  ", c(...), collapse = "\n")
-  if (copy && clipr::clipr_available()) {
-    clipr::write_clip(paste0(c(...), collapse = "\n"))
-    message("Copying code to clipboard:")
-  }
-  cat_line(crayon::make_style("darkgrey")(block))
-}
-
 cat_line <- function(...) {
   cat(..., "\n", sep = "")
-}
-
-field <- function(...) {
-  x <- paste0(...)
-  crayon::green(x)
 }
 
 value <- function(...) {
   x <- paste0(...)
   crayon::blue(encodeString(x, quote = "'"))
-}
-
-code <- function(...) {
-  x <- paste0(...)
-  crayon::make_style("darkgrey")(encodeString(x, quote = "`"))
-}
-
-collapse <- function(x, sep = ", ") {
-  paste0(x, collapse = sep)
 }
