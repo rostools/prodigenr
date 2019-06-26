c92a79173431db54c3b3e204a822356fed978280
Luke Johnston <lwjohnst@gmail.com>
2019-06-26 14:29:58 +0200

    quiet function

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,9 @@
+quiet <- function(x) {
+    # To suppress usethis's messages.
+    ops <- options(usethis.quiet = TRUE)
+    on.exit(options(ops))
+    x
+}
 
 is_rproj_folder <- function() {
     rprojroot::is.root_criterion(rprojroot::is_rstudio_project)
