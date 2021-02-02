9bf079c02732c44c481ba9027fce20ff6aeab81c
Luke Johnston <lwjohnst@gmail.com>
2021-02-02 20:07:14 +0100

    remove import to usethis, roxygen tags added with use_package_doc

--- R/prodigenr-package.R
+++ R/prodigenr-package.R
@@ -1,3 +1,8 @@
 #' @keywords internal
-#' @import usethis
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
