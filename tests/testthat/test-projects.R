ae36b2c7964b2fd5bb243e3ba7ffbcfa11827ffc
Luke W. Johnston <lwjohnst@gmail.com>
2021-04-22 18:07:45 +0200

    update tests

--- tests/testthat/test-projects.R
+++ tests/testthat/test-projects.R
@@ -1,19 +1,39 @@
-context('projects are created')
+context('Project creation and setup.')
+skip_on_cran()
 
-# List of templates -------------------------------------------------------
-
-test_that("the number of listed templates is correct", {
-    expect_length(template_list, 4)
-})
+new_project <- fs::path_temp("testing")
+setup_project(new_project)
 
 # project creation --------------------------------------------------------
 
-
 test_that("project is set up", {
-    skip_on_cran()
-
     expect_true(fs::dir_exists(new_project))
 
     files_created <- fs::dir_ls(new_project, recurse = TRUE)
-    expect_equal(length(grep("/R/", files_created)), 3)
+    folders_created <- fs::dir_ls(new_project, type = "directory")
+    expect_equal(sort(basename(folders_created)),
+                 sort(c("R", "doc", "data", "data-raw")))
+
+    withr::local_dir(new = new_project)
+    expect_true(file.exists("DESCRIPTION"))
+    expect_true(file.exists("testing.Rproj"))
+})
+
+test_that("git gets added", {
+    withr::local_dir(new = new_project)
+    capture_output(setup_project_with_git())
+    expect_true(fs::dir_exists(".git"))
+    expect_true(fs::file_exists(".gitignore"))
+})
+
+fs::dir_delete(new_project)
+
+test_that("project checks work correctly", {
+    expect_error(setup_project(1))
+
+    temp_dir <- tempdir()
+    proj_with_space <- fs::file_temp("test new", tmp_dir = temp_dir)
+    expect_warning(setup_project(proj_with_space))
+    expect_true(fs::file_exists(sub("test new", "test-new", proj_with_space)))
+    fs::dir_delete(sub("test new", "test-new", proj_with_space))
 })
