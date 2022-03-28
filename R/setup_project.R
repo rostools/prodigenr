63892efbbc717471184aacc03c6b105f9905cccb
Anna <anna.schritz@lih.lu>
2022-03-28 13:57:41 +0200

    Adding folder to project setup

--- R/setup_project.R
+++ R/setup_project.R
@@ -41,7 +41,7 @@ setup_project <-
     }
 
 create_directories <- function() {
-    fs::dir_create(c("R", "data", "doc", "data-raw"))
+    fs::dir_create(c("R", "data", "doc", "data-raw", "SAS", "DM", "literature"))
 }
 
 # File inclusion functions --------------------------------------
