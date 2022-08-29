fd97eb30bc276ca1c455fcf0f84495b9a68deab0
Luke W. Johnston <lwjohnst@gmail.com>
2022-08-29 13:54:10 +0200

    fixes to tests

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -6,17 +6,15 @@ setup_project(new_project)
 test_that("Report and slides created", {
     withr::local_dir(new = new_project)
 
-    quiet_test(create_report())
     quiet_test(create_slides())
 
     withr::local_dir(new = file.path(new_project, "doc"))
-    expect_true(file.exists("report.Rmd"))
     expect_true(file.exists("slides.Rmd"))
 
     # Needs a Rproj file.
     file.remove("../testing-docs.Rproj")
-    file.remove("report.Rmd")
-    expect_error(create_report())
+    file.remove("slides.Rmd")
+    expect_error(create_slides())
 })
 
 fs::dir_delete(new_project)
